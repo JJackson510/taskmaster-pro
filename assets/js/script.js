17f79cac649cdb0b3585f67d9d17b6bc69b0b665
JJackson510 <jamiej.jj5@gmail.com>
2021-12-13 21:09:12 -0600

    try hard

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,7 +43,19 @@ var loadTasks = function() {
 
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
+  
 };
+  $(".list-group").on("click", "p", function() {
+    var text = $(this)
+    .text()
+    .trim();
+    var textInput = $("<textarea>")
+    .addClass("form-control")
+    .val(text);
+    $(this).replaceWith(textInput);
+  });
+  
+  
 
 
 
